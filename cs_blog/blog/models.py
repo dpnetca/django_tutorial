4c8139a61d382a47302e39299f479ba13d9bb72f
denis <git@dpnet.ca>
2019-08-17 17:29:01 -0600

    cs_blog completed upto video 13

--- cs_blog/blog/models.py
+++ cs_blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -8,6 +9,9 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
 
     def __str__(self):
         return f"{self.title}"
